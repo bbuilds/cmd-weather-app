c85c88ed5b4f806533e828baa2dd04684dfc45a7
Branden Builds <hello@brandenbuilds.com>
2017-10-04 18:21:28 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+//stores final parsed output
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        },
+        c: {
+            demand: false,
+            alias: 'format',
+            describe: 'format C for Celcius. Defaults to Farhenheit.',
+            string: true
+        }
+    })
+    .help()
+    .argv;
+
+const weatherAPiKey = '87d86478032a88b856080cba730f07f6';
+const endcodedAddress = encodeURIComponent(argv.address);
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${endcodedAddress}`;
+const convertCelcius = argv.c;
+
+axios.get(geocodeUrl).then((response) => {
+    if(response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address');
+    }
+    const lat = response.data.results[0].geometry.location.lat;
+    const long = response.data.results[0].geometry.location.lng;
+    const weatherUrl = `https://api.darksky.net/forecast/${weatherAPiKey}/${lat},${long}`;
+
+    console.log(response.data.results[0].formatted_address);
+    
+    return axios.get(weatherUrl);  
+
+})
+.then((response) => {
+    const temp = response.data.currently.temperature;
+    const apprantTemp = response.data.currently.apparentTemperature;
+    const celciusTemp = Math.round(((response.data.currently.temperature - 32) / 9) * 5, 2);
+    const celciusApprantTemp = Math.round(((response.data.currently.apparentTemperature - 32) / 9) * 5, 2);
+
+    if (convertCelcius) {
+        console.log(`The temp says ${celciusTemp} outside but it feels like ${celciusApprantTemp}`);
+    } else {
+        console.log(`The temp says ${temp} outside but it feels like ${apprantTemp}`);
+    }
+})
+.catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+      console.log('Unable to connect to API servers.');
+    } else {
+      console.log(e.message);
+    }  
+})
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Command Line Weather App
+
+A simple command line app using Dark Sky and Google Location APIs to fetch location and weather data.
+
+## Getting Started
+
+```
+npm install
+
+```
+
+## Running the app
+
+Set up an API Key with [Dark Sky](https://darksky.net/dev) and update variable.
+
+Open your command prompt
+
+```
+node app.js -a="1234 Valid Address Dr." 
+```
+Pass the c argument to convert temperature from Fahrenheit to Celsius
+
+```
+node app.js -a="1234 Valid Address Dr."  -c=c
+```
+
